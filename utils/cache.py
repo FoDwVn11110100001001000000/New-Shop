ec596a97489babf7f1de3f1bf631080a5e0da56e
DFes <mattiabianchi77@gmail.com>
2025-06-11 02:25:39 +0300

    refactoring / add docstings

--- utils/cache.py
+++ utils/cache.py
@@ -13,6 +13,16 @@ class RedisManager:
         db=0,
         decode_responses=True
     ):
+        """
+        Constructor for RedisManager class.
+
+        Args:
+            host (str): Redis host. Defaults to config.redis_host.
+            port (int): Redis port. Defaults to config.redis_port.
+            expire (int): Expiration time in seconds. Defaults to config.redis_expire.
+            db (int, optional): Redis database number. Defaults to 0.
+            decode_responses (bool, optional): If True, all responses will be decoded with utf-8. Defaults to True.
+        """
         self.client = redis.Redis(
             host=host,
             port=port,
@@ -22,6 +32,12 @@ class RedisManager:
         self.expire = expire
 
     async def connect(self):
+        """
+        Checks the connection to Redis.
+
+        Tries to ping Redis server and log the result.
+        If the connection fails, logs an error and raises the exception.
+        """
         try:
             await self.client.ping()
             log.info("✅ Success connection to Redis")
@@ -30,13 +46,31 @@ class RedisManager:
             raise
 
     async def reserve_lots(self, telegram_id: int, lots: list[dict]) -> None:
+        """
+        Reserves lots for a given telegram_id.
+
+        Args:
+            telegram_id (int): Telegram user ID.
+            lots (list[dict]): List of dictionaries with lot data.
+
+        Returns:
+            None
+        """
         value = json.dumps(lots)
         await self.client.set(name=telegram_id, value=value, ex=self.expire)
         log.info(f"ID: {telegram_id}| Reserved lots: {value}")
 
 
     async def get_reserved_by_user(self, telegram_id: int) -> list[dict]:
-        """Получает список зарезервированных лотов по telegram_id"""
+        """
+        Retrieves all reserved lots for a given telegram_id.
+
+        Args:
+            telegram_id (int): Telegram user ID.
+
+        Returns:
+            list[dict]: List of dictionaries with lot data if found, else an empty list.
+        """
         raw = await self.client.get(telegram_id)
         if raw:
             log.info(f"ID: {telegram_id}| Get reserved lots by user: {raw}")
@@ -44,7 +78,18 @@ class RedisManager:
         return []
 
     async def get_all_reserved_by_types(self, telegram_id: str, type: str) -> list[str]:
-        """Получает все type из всех зарезервированных лотов"""
+        """
+        Retrieves all reserved lots for a given telegram_id and type.
+
+        Args:
+            telegram_id (str): Telegram user ID.
+            type (str): Type of the lot to retrieve.
+
+        Returns:
+            list[str]: List of strings with the type of the lot for the given
+                telegram_id and type if found, else an empty list.
+        """
+
         keys = await self.client.keys(f"{telegram_id}")
         types = []
         for key in keys:
@@ -55,7 +100,17 @@ class RedisManager:
         return types
 
     async def get_all_reserved_types(self, telegram_id: str) -> list[dict[str, str]]:
-        """Получает все зарезервированные лоты (список словарей) по telegram_id"""
+        """
+        Retrieves all reserved lot types for a given telegram_id.
+
+        Args:
+            telegram_id (str): Telegram user ID.
+
+        Returns:
+            list[dict[str, str]]: A list of dictionaries containing lot type data
+            associated with the given telegram_id. If no data is found, returns an
+            empty list.
+        """
         keys = await self.client.keys(f"{telegram_id}")
         types = []
         for key in keys:
